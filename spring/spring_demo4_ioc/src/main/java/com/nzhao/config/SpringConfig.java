9261e49d6c0203a5a9f2b972a9cc451def9dff4f
010642 <nzhao@sudytech.com>
2020-10-30 16:28:25 +0800

    Spring纯注解方式配置Druid连接池

---
+++ spring/spring_demo4_ioc/src/main/java/com/nzhao/config/SpringConfig.java
@@ -0,0 +1,38 @@
+package com.nzhao.config;
+
+import com.alibaba.druid.pool.DruidDataSource;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+
+import javax.sql.DataSource;
+
+@Configuration//声明
+@PropertySource(value = "classpath:jdbc.properties")//@PropertySource能加载properties文件
+public class SpringConfig {
+
+    //从配置文件jdbc.properties取出设置数据源的参数
+    @Value("${jdbc.url}")
+    String url;
+    @Value("${jdbc.driver}")
+    String driver;
+    @Value("${jdbc.username}")
+    String username;
+    @Value("${jdbc.password}")
+    String password;
+
+
+    @Bean(name = "dataSource")//设置数据源Druid
+    public DataSource getDataSource(){
+        //创建数据源
+        DruidDataSource dataSource = new DruidDataSource();
+        //设置数据源连接参数
+        dataSource.setUrl(url);
+        dataSource.setDriverClassName(driver);
+        dataSource.setUsername(username);
+        dataSource.setPassword(password);
+        //返回数据源
+        return dataSource;
+    }
+}
---
+++ spring/spring_demo4_ioc/src/test/java/com/nzhao/DruidTest.java
@@ -0,0 +1,20 @@
+package com.nzhao;
+
+import com.nzhao.config.SpringConfig;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.SQLException;
+
+public class DruidTest {
+    public static void main(String[] args) throws SQLException {
+        //通过Spring获取Druid连接池对象
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(SpringConfig.class);
+        DataSource dataSource = applicationContext.getBean(DataSource.class);
+        //对Druid连接池对象进行操作测试
+        Connection connection = dataSource.getConnection();
+        System.out.println("连接数据源测试 "+connection);
+    }
+}
