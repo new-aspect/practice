23838bfdaa4b5d49ce9c404063f2009bfe16b7a4
010642 <nzhao@sudytech.com>
2020-11-03 10:08:12 +0800

    搭建spring整合Mybatis的基础练习

---
+++ spring/spring_demo14_mybatis/src/test/java/com/nzhao/service/AccountServiceTest.java
@@ -0,0 +1,22 @@
+package com.nzhao.service;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(value = SpringJUnit4ClassRunner.class)
+@ContextConfiguration(value = {"classpath:applicationContext.xml"})
+public class AccountServiceTest {
+
+    @Autowired
+    AccountService accountService;
+
+
+    @Test
+    public void test(){
+        System.out.println(accountService.findAll());
+    }
+
+}
