cda538d8b5f46090bc2a34754fad062aababaae3
010642 <nzhao@sudytech.com>
2020-11-12 17:26:59 +0800

    使用Jackson提供的ObjectMapper，将Java对象转换为json

--- spring/springMVC_parent01/springmvc-05-json/src/main/java/com/nzhao/controller/HelloController.java
+++ spring/springMVC_parent01/springmvc-05-json/src/main/java/com/nzhao/controller/HelloController.java
@@ -1,5 +1,7 @@
 package com.nzhao.controller;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.nzhao.dao.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,11 +12,15 @@ public class HelloController {
 
     @RequestMapping("/hello")
     @ResponseBody// 添加ResponseBody就不会走视图解析器，会直接返回一个字符串
-    public String hello(){
+    public String hello() throws JsonProcessingException {
 
         //创建User对象，查看直接返回字符串的效果
         User user = new User(1, "nzhao", 33);
 
-        return user.toString();
+        //使用Jackson提供的ObjectMapper，将Java对象转换为json
+        ObjectMapper mapper = new ObjectMapper();
+        String json = mapper.writeValueAsString(user);
+
+        return json;
     }
 }
