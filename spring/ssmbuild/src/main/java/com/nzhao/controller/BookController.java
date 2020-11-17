6aa0dc9465b047049009d950aa5a882b96047886
010642 <nzhao@sudytech.com>
2020-11-17 16:46:09 +0800

    简单修改一个视图

--- spring/ssmbuild/src/main/java/com/nzhao/controller/BookController.java
+++ spring/ssmbuild/src/main/java/com/nzhao/controller/BookController.java
@@ -1,11 +1,15 @@
 package com.nzhao.controller;
 
+import com.nzhao.pojo.Books;
 import com.nzhao.service.BookService;
 import com.nzhao.service.impl.BookServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 @RequestMapping("/book")
 @Controller
@@ -15,8 +19,15 @@ public class BookController {
     private BookService bookService;
 
 
+    /**
+     * 查询书籍的全部信息，转发视图
+     * @param model
+     * @return
+     */
     @RequestMapping("/allBook")
-    public String allBook(){
+    public String allBook(Model model){
+        List<Books> booksList = bookService.queryAllBook();
+        model.addAttribute("list",booksList);
         return "allBook";
     }
 }
