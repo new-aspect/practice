a3a20943f5900365605f6d51b521b580bf640b27
010642 <nzhao@sudytech.com>
2020-11-19 14:35:50 +0800

    spring整合Junit1进行测试

--- spring/ssmbuild2/src/test/java/DaoTest.java
+++ spring/ssmbuild2/src/test/java/DaoTest.java
@@ -1,17 +1,27 @@
 import com.nzhao.dao.BookMapping;
 import com.nzhao.pojo.Books;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import java.util.List;
 
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(value = "classpath:applicationContext.xml")
 public class DaoTest {
+
+    @Autowired
+    BookMapping bookMapping;
 
     @Test
     public void test(){
-        ApplicationContext applicationContext = new ClassPathXmlApplicationContext("applicationContext.xml");
-        BookMapping bookMapping = applicationContext.getBean(BookMapping.class);
+//        ApplicationContext applicationContext = new ClassPathXmlApplicationContext("applicationContext.xml");
+//        BookMapping bookMapping = applicationContext.getBean(BookMapping.class);
         List<Books> books = bookMapping.queryAll();
         System.out.println(books);
     }
