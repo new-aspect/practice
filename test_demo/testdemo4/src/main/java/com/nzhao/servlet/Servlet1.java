bc51836547e45de889dee3020fd6eeceb9bc4ce7
010642 <nzhao@sudytech.com>
2021-01-14 10:05:50 +0800

    测试ServletContext

--- test_demo/testdemo4/src/main/java/com/nzhao/servlet/Servlet1.java
+++ test_demo/testdemo4/src/main/java/com/nzhao/servlet/Servlet1.java
@@ -1,28 +1,31 @@
 package com.nzhao.servlet;
 
+import javax.servlet.Servlet;
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.io.PrintWriter;
 
 /**
- * @author 11507
+ * 测试ServletContext共享Servlet数据，需要两个Servlet，
+ * 一个是Servlet1，一个是Servlet2
+ * @author nzhao
  */
-public class HelloServlet extends HttpServlet {
+public class Servlet1 extends HttpServlet {
+
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        ServletContext servletContext = this.getServletContext();
-        // 新建一个数据
-        String username = "张三";
-        // 将这个数据放入servletContext中
-        servletContext.setAttribute("username",username);
+        String name = "李四";
+
+        // 将一个数据放入ServletContext
+        ServletContext servletContext = req.getServletContext();
+        servletContext.setAttribute("name",name);
     }
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        super.doPost(req, resp);
+        this.doGet(req, resp);
     }
 }
--- test_demo/testdemo4/src/main/java/com/nzhao/servlet/Servlet2.java
+++ test_demo/testdemo4/src/main/java/com/nzhao/servlet/Servlet2.java
@@ -6,23 +6,25 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.io.PrintWriter;
 
 /**
- * @author 11507
+ * 测试ServletContext共享Servlet数据，需要两个Servlet，
+ * 一个是Servlet1，一个是Servlet2
+ * @author nzhao
  */
-public class HelloServlet extends HttpServlet {
+public class Servlet2 extends HttpServlet {
+
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        ServletContext servletContext = this.getServletContext();
-        // 新建一个数据
-        String username = "张三";
-        // 将这个数据放入servletContext中
-        servletContext.setAttribute("username",username);
+        // 从ServletContext获得数据
+        ServletContext servletContext = req.getServletContext();
+        String name = (String) servletContext.getAttribute("name");
+
+        System.out.println(" name = " + name);
     }
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        super.doPost(req, resp);
+        this.doGet(req, resp);
     }
 }
