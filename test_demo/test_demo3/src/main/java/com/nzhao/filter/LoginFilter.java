b12a2ff7d122803cb6572709722a63b7fdf9e31f
010642 <nzhao@sudytech.com>
2021-02-04 18:02:05 +0800

    配置过滤器实现sys目录的文件必须登录需求

---
+++ test_demo/test_demo3/src/main/java/com/nzhao/filter/LoginFilter.java
@@ -0,0 +1,34 @@
+package com.nzhao.filter;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * @author 11507
+ */
+public class LoginFilter implements Filter {
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+
+    }
+
+    @Override
+    public void doFilter(ServletRequest req, ServletResponse resp, FilterChain chain) throws IOException, ServletException {
+        HttpServletRequest request = (HttpServletRequest) req;
+        HttpServletResponse response = (HttpServletResponse) resp;
+        if(request.getSession().getAttribute("USER_SESSION") == null){
+//            response.sendRedirect("/example/login.html");
+            response.sendRedirect("/example/error.html");
+        }
+
+        // 让过滤器继续执行下去
+        chain.doFilter(req, resp);
+    }
+
+    @Override
+    public void destroy() {
+
+    }
+}
