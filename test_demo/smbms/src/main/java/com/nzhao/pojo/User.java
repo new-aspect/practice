018f02bcb3bc4c1e2f805b6ebe50ded9c16df7a7
010642 <nzhao@sudytech.com>
2021-03-19 16:45:44 +0800

    写一个ssmbms环境

---
+++ test_demo/smbms/src/main/java/com/nzhao/pojo/User.java
@@ -0,0 +1,198 @@
+package com.nzhao.pojo;
+
+import java.util.Date;
+
+/**
+ * 对应数据库的用户类
+ * @author nzhao
+ */
+public class User {
+
+    /**
+     * 主键ID
+     */
+    private Integer id;
+
+    /**
+     * 用户编码
+     */
+    private String userCode;
+
+    /**
+     * 用户名称
+     */
+    private String userName;
+
+    /**
+     * 用户密码
+     */
+    private String userPassword;
+
+    /**
+     * 性别（1:女、 2:男）
+     */
+    private Integer gender;
+
+    /**
+     * 出生日期
+     */
+    private Date birthday;
+
+    /**
+     * 手机
+     */
+    private String phone;
+
+    /**
+     * 地址
+     */
+    private String address;
+
+    /**
+     * 用户角色（取自角色表-角色id）
+     */
+    private Integer userRole;
+
+    /**
+     * 创建者(userID)
+     */
+    private Integer createBy;
+
+    /**
+     * 创建时间
+     */
+    private Date creationDate;
+
+    /**
+     * 更新者（userID）
+     */
+    private Integer modifyBy;
+
+    /**
+     * 更新时间
+     */
+    private Date modifyDate;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getUserCode() {
+        return userCode;
+    }
+
+    public void setUserCode(String userCode) {
+        this.userCode = userCode;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getUserPassword() {
+        return userPassword;
+    }
+
+    public void setUserPassword(String userPassword) {
+        this.userPassword = userPassword;
+    }
+
+    public Integer getGender() {
+        return gender;
+    }
+
+    public void setGender(Integer gender) {
+        this.gender = gender;
+    }
+
+    public Date getBirthday() {
+        return birthday;
+    }
+
+    public void setBirthday(Date birthday) {
+        this.birthday = birthday;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public Integer getUserRole() {
+        return userRole;
+    }
+
+    public void setUserRole(Integer userRole) {
+        this.userRole = userRole;
+    }
+
+    public Integer getCreateBy() {
+        return createBy;
+    }
+
+    public void setCreateBy(Integer createBy) {
+        this.createBy = createBy;
+    }
+
+    public Date getCreationDate() {
+        return creationDate;
+    }
+
+    public void setCreationDate(Date creationDate) {
+        this.creationDate = creationDate;
+    }
+
+    public Integer getModifyBy() {
+        return modifyBy;
+    }
+
+    public void setModifyBy(Integer modifyBy) {
+        this.modifyBy = modifyBy;
+    }
+
+    public Date getModifyDate() {
+        return modifyDate;
+    }
+
+    public void setModifyDate(Date modifyDate) {
+        this.modifyDate = modifyDate;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", userCode='" + userCode + '\'' +
+                ", userName='" + userName + '\'' +
+                ", userPassword='" + userPassword + '\'' +
+                ", gender=" + gender +
+                ", birthday=" + birthday +
+                ", phone='" + phone + '\'' +
+                ", address='" + address + '\'' +
+                ", userRole=" + userRole +
+                ", createBy=" + createBy +
+                ", creationDate=" + creationDate +
+                ", modifyBy=" + modifyBy +
+                ", modifyDate=" + modifyDate +
+                '}';
+    }
+}
