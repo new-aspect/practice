2ab94ad1dc695ad73894325af1cda1be9283d0e2
010642 <nzhao@sudytech.com>
2021-03-25 16:43:09 +0800

    字符串过滤

---
+++ test_demo/smbms/src/main/java/com/nzhao/filter/CharacterEncodingFilter.java
@@ -0,0 +1,27 @@
+package com.nzhao.filter;
+
+import javax.servlet.*;
+import java.io.IOException;
+
+/**
+ * 字符串过滤
+ * @author 11507
+ */
+public class CharacterEncodingFilter implements Filter {
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+
+    }
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        request.setCharacterEncoding("utf-8");
+        response.setCharacterEncoding("utf-8");
+        chain.doFilter(request, response);
+    }
+
+    @Override
+    public void destroy() {
+
+    }
+}
