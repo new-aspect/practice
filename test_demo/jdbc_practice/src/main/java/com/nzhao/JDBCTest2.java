46d133373f400a24048f225d6017609f2abe072f
010642 <nzhao@sudytech.com>
2021-05-08 15:38:46 +0800

    完善jdbc

--- test_demo/jdbc_practice/src/main/java/com/nzhao/JDBCTest2.java
+++ test_demo/jdbc_practice/src/main/java/com/nzhao/JDBCTest2.java
@@ -2,6 +2,10 @@ package com.nzhao;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.Properties;
 
 /**
@@ -14,12 +18,26 @@ public class JDBCTest2 {
          */
         InputStream inputStream = JDBCTest2.class.getClassLoader().getResourceAsStream("db.properties");
         Properties properties = new Properties();
+        String sql = "select * from table1";
         try {
+            // 获取jdbc所需的参数
             properties.load(inputStream);
-        } catch (IOException e) {
+            String username = properties.getProperty("username");
+            String password = properties.getProperty("password");
+            String url = properties.getProperty("url");
+
+            // 操作jdbc
+            Connection connection = DriverManager.getConnection(url, username, password);
+            ResultSet resultSet = connection.prepareStatement(sql).executeQuery();
+            while (resultSet.next()) {
+                int a = resultSet.getInt("a");
+                String b = resultSet.getString("b");
+                float c = resultSet.getFloat("c");
+                System.out.println("a = " + a + " b = " + b + " c = " + c);
+            }
+            connection.close();
+        } catch (IOException | SQLException e) {
             e.printStackTrace();
         }
-        System.out.println(properties.getProperty("username"));
-
     }
 }
